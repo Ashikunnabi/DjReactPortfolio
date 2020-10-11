e6e12c24bed97d66a2be5d6a21d57580a45ac92f
Ashikunnabi <ashikunnabituhin@gmail.com>
2020-10-11 20:42:42 +0600

    card style fixed

--- frontend/src/components/base/Navbar.js
+++ frontend/src/components/base/Navbar.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
-MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon } from "mdbreact";
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse } from "mdbreact";
+import { Route } from 'react-router-dom';
 
 import { Home, Portfolio, Blog } from '../pages/';
 
@@ -16,10 +15,10 @@ class Navbar extends Component {
 
     render() {
       return (
-        <Router>
+        <>
           <MDBNavbar color="default-color sticky-top" dark expand="md">
             <MDBNavbarBrand>
-              <strong className="white-text">Nabi</strong>
+              <strong className="white-text">Ashikun Nabi</strong>
             </MDBNavbarBrand>
             <MDBNavbarToggler onClick={this.toggleCollapse} />
             <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
@@ -78,7 +77,7 @@ class Navbar extends Component {
           <Route exact path="/" component={Home} />
           <Route exact path="/Portfolio" component={Portfolio} />
           <Route exact path="/Blog" component={Blog} />
-        </Router>
+        </>
         );
       }
 }
