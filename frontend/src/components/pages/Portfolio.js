2fb8e8b21c555546b379d0da943356cefa26b4c9
Md. Ashikun Nabi <ashikunnabituhin@gmail.com>
2021-01-25 07:38:25 +0600

    portfolio status based return added

--- frontend/src/components/pages/Portfolio.js
+++ frontend/src/components/pages/Portfolio.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import { fetchPortfolioApiCall } from '../../redux/portfolio/action'
 
 const Portfolio = (props) => {
-  const { projects, nextURL } = props.data_portfolios
+  const { projects, nextURL, status } = props.data_portfolios
   const { get_portfolios } = props
   const [seeDetail, setSeeDetail] = React.useState(null);
   const [showModal, setShowModal] = React.useState(false);
@@ -23,27 +23,51 @@ const Portfolio = (props) => {
   const toggle = () => {
     setShowModal(!showModal)
   }
+  if (status === 'progress') {
+    return (
+      <Fragment >
+        <div className="container-fluid">
+          <img
+            src={require('../../assets/base/loading1.gif')}
+            className="img-fluid shadow-box-example z-depth-1"
+            alt="loading"
+            width="100%"
+          />
+        </div>
+      </Fragment>
+    )
+  }
+  if (status === 'success') {
+    return (
+      <Fragment >
+        <div className="container-fluid Home__background_image">
+          <MDBRow>
+            {projects && projects.map(x => <Card key={x.id} obj={x} seeProjectDetails={seeDetails} modal={toggle} />)}
 
-  return (
-    <Fragment >
-      <div className="container-fluid Home__background_image">
-        <MDBRow>
-          {projects && projects.map(x => <Card key={x.id} obj={x} seeProjectDetails={seeDetails} modal={toggle} />)}
-
-        </MDBRow>
-        {nextURL && (
-          <MDBRow style={{ marginTop: '20px', justifyContent: 'center' }}>
-            <MDBBtn outline color="black" onClick={showMore}><strong>MORE</strong></MDBBtn>
           </MDBRow>
-        )
-        }
-        {showModal && (
-          <FrameModal toggleModal={toggle} obj={JSON.parse(seeDetail)} modal={9} />
-        )
-        }
-      </div>
-    </Fragment>
-  )
+          {nextURL && (
+            <MDBRow style={{ marginTop: '20px', justifyContent: 'center' }}>
+              <MDBBtn outline color="black" onClick={showMore}><strong>MORE</strong></MDBBtn>
+            </MDBRow>
+          )
+          }
+          {showModal && (
+            <FrameModal toggleModal={toggle} obj={JSON.parse(seeDetail)} modal={9} />
+          )
+          }
+        </div>
+      </Fragment>
+    )
+  }
+  if (status === 'failed') {
+    return (
+      <Fragment >
+        <div className="container-fluid Home__background_image text-center vh-100">
+          <p>Something went wrong!</p>
+        </div>
+      </Fragment>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
